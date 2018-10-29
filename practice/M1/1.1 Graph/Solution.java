aa4492300c8ade113049a4cfe8e5f3ed8c594df4
chinuteja <chinuteja2008@msitprogram.net>
2018-10-29 23:45:32 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ practice/M1/1.1 Graph/Solution.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public final class Solution {
+	private Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String type = scan.nextLine();
+		int vertices = scan.nextInt();
+		int edges = scan.nextInt();
+		scan.nextLine();
+		LinearProbingHashST<Integer, String> hashst = new LinearProbingHashST<>();
+		String[] places = scan.nextLine().split(",");
+		for (int i = 0; i < places.length; i++) {
+			hashst.put(i, places[i]);
+		}
+		switch (type) {
+		case "List":
+			Graph graph = new Graph(vertices);
+			while (scan.hasNext()) {
+				String[] tokens = scan.nextLine().split(" ");
+				graph.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+			}
+
+			System.out.println(graph.V() + " vertices," + graph.E() + "edges");
+			for (int i = 0; i < graph.V(); i++) {
+				StringBuffer str = new StringBuffer();
+				str.append(hashst.get(i) + ":");
+				for (int w : graph.adj(i)) {
+					str.append(hashst.get(w) + " ");
+				}
+				System.out.println(str.toString().trim());
+			}
+
+		}
+		//System.out.println(type);
+
+	}
+
+
+}

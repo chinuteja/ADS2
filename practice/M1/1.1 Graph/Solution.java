bdf9f46942d968b85522b5ba784c33a89fcdbf76
chinuteja <chinuteja2008@msitprogram.net>
2018-10-29 23:57:44 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/M1/1.1 Graph/Solution.java
+++ practice/M1/1.1 Graph/Solution.java
@@ -23,6 +23,8 @@ public final class Solution {
 			}
 
 			System.out.println(graph.V() + " vertices, " + graph.E() + " edges");
+			if(graph.E() == 0)
+				break;
 			for (int i = 0; i < graph.V(); i++) {
 				StringBuffer str = new StringBuffer();
 				str.append(hashst.get(i) + ": ");

f56891a15df3b88b0a583e5bf0cbb8acf21b9f99
chinuteja <chinuteja2008@msitprogram.net>
2018-10-30 19:36:33 +0530

    "ads2_m1_a1_1: 5/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/M1/1.1 Graph/Solution.java
+++ practice/M1/1.1 Graph/Solution.java
@@ -62,14 +62,16 @@ public final class Solution {
 				System.out.println("No edges");
 				break;
 			}
+			while(scan.hasNext()){
+				String[] tokens2 = scan.nextLine().split(" ");
 			for (int i = 0; i < matrixgraph.V(); i++) {
 				String str = "";
 				for (int j = 0; j < matrixgraph.V(); j++) {
-					str = str + matrixgraph.contains(1,3) + "";
+					str = str + matrixgraph.contains(Integer.parseInt(tokens2[0]),Integer.parseInt(tokens2[1]))+ "";
 				}
 				System.out.println(str);
 				System.out.println();
-			}
+			}}
 
 			break;
 

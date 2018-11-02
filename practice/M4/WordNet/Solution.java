f99b8ea8a1d1b75da50531cad61b28a47e32e343
chinuteja <chinuteja2008@msitprogram.net>
2018-11-02 18:07:53 +0530

    "ads2_m4_a1_1: 7/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/M4/WordNet/Solution.java
+++ practice/M4/WordNet/Solution.java
@@ -1,26 +1,27 @@
 import java.util.Scanner;
 class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String synsetsFileName = scan.nextLine();
-		String hypernymsFileName = scan.nextLine();
-		WordNet wordNet = new WordNet("\\Files\\" + synsetsFileName, "\\Files\\" + hypernymsFileName);
-		//LinearProbingHashST<> hashst = 
-		String line = scan.nextLine();
-		switch (line) {
-		case "Graph":
-			wordNet.printGraph();
-			break;
-		case "Queries":
-		    while(scan.hasNext()) {
-		    	String[] tokens1 = scan.nextLine().split(" ");
-		        if (tokens1[0].equals("null") || tokens1[1].equals("null")) {
-		        	System.out.println("IllegalArgumentException");
-		        }
-		    }
-			break;
-		default:
-			break;
-		}
-	}
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String synsetsFileName = scan.nextLine();
+        String hypernymsFileName = scan.nextLine();
+        WordNet wordNet = new WordNet("\\Files\\" + synsetsFileName,
+                                      "\\Files\\" + hypernymsFileName);
+        //LinearProbingHashST<> hashst =
+        String line = scan.nextLine();
+        switch (line) {
+        case "Graph":
+            wordNet.printGraph();
+            break;
+        case "Queries":
+            while (scan.hasNext()) {
+                String[] tokens1 = scan.nextLine().split(" ");
+                if (tokens1[0].equals("null") || tokens1[1].equals("null")) {
+                    System.out.println("IllegalArgumentException");
+                }
+            }
+            break;
+        default:
+            break;
+        }
+    }
 }

479ebf0c505639cdfbde10023e8351463b445728
chinuteja <chinuteja2008@msitprogram.net>
2018-11-03 12:16:14 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/ADS-2Week-1/ADS-2Week-1/PageRank.java
+++ practice/ADS-2Week-1/ADS-2Week-1/PageRank.java
@@ -1,14 +1,32 @@
 
 class PageRank {
 	Digraph digraph;
+	double[] rank;
+	double[] prevrank;
 	PageRank(Digraph digraph) {
 		this.digraph = digraph;
+		int vertices = digraph.numberofVertices();
+		prevrank = new double[vertices];
+		rank = new double[vertices];
+		for (int i = 0; i < vertices; i++) {
+			rank[i] = (double) 1 / vertices;
+		}
 	}
 	public double getPR(int vertex) {
-		return 0;
+		double sum = 0;
+		for (int i :digraph.adj(vertex) ) {
+			if (digraph.outdegree(i) == 0) {
+				return 0;
+			}
+			else {
+				sum += prevrank[i] / digraph.outdegree(i);
+			}
+		}
+		return rank[vertex] = sum;
 	}
 	public  double getCalculation() {
-		double intialvalue = (1/digraph.numberofVertices());
+		double intialvalue = (1 / digraph.numberofVertices());
+		//System.out.println(digraph.numberofVertices());
 		System.out.println(intialvalue);
 		for (int j = 0 ; j < 1000; j++) {
 			for (int i = 0; i < digraph.numberofVertices() ; i++) {

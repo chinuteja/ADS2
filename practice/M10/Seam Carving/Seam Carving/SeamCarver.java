1cc02fb9811c0b9daac90249f3d722844d994f65
chinuteja <chinuteja2008@msitprogram.net>
2018-11-08 22:48:23 +0530

    "ads2_m10_a1_1: 5/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/M10/Seam Carving/Seam Carving/SeamCarver.java
+++ practice/M10/Seam Carving/Seam Carving/SeamCarver.java
@@ -1,10 +1,10 @@
-
+import java.awt.Color;
 public class SeamCarver {
 	// create a seam carver object based on the given picture
 	Picture picture;
+	//double[][] energy;
 	public SeamCarver(Picture picture1) {
 		picture = new Picture(picture1);
-
 	}
 	// current picture
 	public Picture picture() {
@@ -14,16 +14,31 @@ public class SeamCarver {
 	public int width() {
 		return picture.width();
 	}
-
 	// height of current picture
 	public int height() {
 		return picture.height();
 	}
-
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
-		double energy = ((x*x) + (y*y))/Math.sqrt((x*x)+(y*y));
-		return energy;
+		if (x == 0 || y == 0 || x == picture.width() - 1 ||
+		        y == picture.height() - 1) {
+			return 1000;
+		} else {
+			Color left = picture.get(x, y - 1);
+			Color right = picture.get(x, y + 1);
+			Color top = picture.get(x - 1, y);
+			Color bottom = picture.get(x + 1, y);
+			int rh = bottom.getRed() - top.getRed();
+			int bh = bottom.getBlue() - top.getBlue();
+			int gh = bottom.getGreen() - top.getGreen();
+			int horizontal = rh * rh + bh * bh + gh * gh;
+			int rv = left.getRed() - right.getRed();
+			int bv = left.getBlue() - right.getBlue();
+			int gv = left.getGreen() - right.getGreen();
+			int vertical = rv * rv + bv * bv + gv * gv;
+			double enr = Math.sqrt(horizontal + vertical);
+			return enr;
+		}
 	}
 
 	// sequence of indices for horizontal seam

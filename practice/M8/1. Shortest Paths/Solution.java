f27a9f24334cff9eafc453e154e362ad5eb5f7c7
chinuteja <chinuteja2008@msitprogram.net>
2018-11-08 22:07:01 +0530

    "ads2_m8_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- practice/M8/1. Shortest Paths/Solution.java
+++ practice/M8/1. Shortest Paths/Solution.java
@@ -13,6 +13,11 @@ public final class Solution {
     private Solution() {
 
     }
+    /**.
+     * main funciton 
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         String[] tokens = scan.nextLine().split(" ");
@@ -21,12 +26,12 @@ public final class Solution {
         int vertices = Integer.parseInt(tokens[0]);
         int edges = Integer.parseInt(tokens[1]);
         String[] stations = scan.nextLine().split(" ");
-        for (int i = 0; i < stations.length; i++ ) {
+        for (int i = 0; i < stations.length; i++) {
             hashst.put(stations[i], i);
         }
         EdgeWeightedGraph edgeweightedgraph =
             new EdgeWeightedGraph(vertices);
-        for (int i = 0; i < vertices ; i++ ) {
+        for (int i = 0; i < vertices; i++) {
             String[] tokens1 = scan.nextLine().split(" ");
             Edge edge = new Edge(hashst.get(tokens[0]),
                                  hashst.get(tokens[1]),
@@ -34,7 +39,7 @@ public final class Solution {
             edgeweightedgraph.addEdge(edge);
         }
         int quires = scan.nextInt();
-        for (int i = 0; i < quires ; i++ ) {
+        for (int i = 0; i < quires; i++) {
             String[] tokens2 = scan.nextLine().split(" ");
             DijkstraSP dijkstrasp =
                 new DijkstraSP(edgeweightedgraph, hashst.get(tokens2[0]));

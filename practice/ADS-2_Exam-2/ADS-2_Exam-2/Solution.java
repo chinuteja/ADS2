14abd07de8980b1ed2284a865ee65711a619fd0a
chinuteja <chinuteja2008@msitprogram.net>
2018-11-10 17:38:24 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- practice/ADS-2_Exam-2/ADS-2_Exam-2/Solution.java
+++ practice/ADS-2_Exam-2/ADS-2_Exam-2/Solution.java
@@ -67,10 +67,12 @@ public final class Solution {
                 System.out.println(c1 + c2);
                 // StringBuffer sb = new StringBuffer();
                 // sb.append(Integer.parseInt(tokens2[0]) + " ");
-                // for (Edge e : dijkstra1.pathTo(Integer.parseInt(tokens2[1]))) {
+                // for (Edge e : dijkstra1.pathTo
+                // (Integer.parseInt(tokens2[1]))) {
                 //     sb.append(e.either() + " ");
                 // }
-                // for (Edge e : dijkstra2.pathTo(Integer.parseInt(tokens2[2]))) {
+                // for (Edge e : dijkstra2.pathTo
+                // (Integer.parseInt(tokens2[2]))) {
                 //      v = e.other(v);
                 //      sb.append(v + " ");
                 // }

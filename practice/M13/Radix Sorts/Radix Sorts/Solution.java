2fb9c6e34550d086e6005a5cbdf6c97d0ddd148f
chinuteja <chinuteja2008@msitprogram.net>
2018-11-12 15:58:27 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/M13/Radix Sorts/Radix Sorts/Solution.java
+++ practice/M13/Radix Sorts/Radix Sorts/Solution.java
@@ -6,35 +6,34 @@ import java.util.Scanner;
  * class for Solution
  */
 public final class Solution {
-	/**.
-	 * Constructs the object.
-	 * @author Teja
-	 */
-	private Solution() {
+    /**.
+     * Constructs the object.
+     * @author Teja
+     */
+    private Solution() {
 
-	}
-	/**.
-	 * main function which performs operations
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int noofstrings = scan.nextInt();
-		scan.nextLine();
-		String[] a = new String[noofstrings];
-		Quick3string quick3string = new Quick3string();
-		for (int i = 0; i < noofstrings; i++) {
-			a[i] = scan.nextLine();
-		}
-		quick3string.sort(a);
-		String str = "[";
-		int i;
-		for ( i = 0; i < noofstrings - 1 ; i++ ) {
-			
-			str = str + a[i] + ", ";
-		}
-		str = str + a[i] + "]";
-		System.out.println(str);
-	}
+    }
+    /**.
+     * main function which performs operations
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int noofstrings = scan.nextInt();
+        scan.nextLine();
+        String[] a = new String[noofstrings];
+        Quick3string quick3string = new Quick3string();
+        for (int i = 0; i < noofstrings; i++) {
+            a[i] = scan.nextLine();
+        }
+        quick3string.sort(a);
+        String str = "[";
+        int i;
+        for (i = 0; i < noofstrings - 1 ; i++) {            
+            str = str + a[i] + ", ";
+        }
+        str = str + a[i] + "]";
+        System.out.println(str);
+    }
 }

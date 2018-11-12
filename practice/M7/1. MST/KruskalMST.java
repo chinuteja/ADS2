9b2b0bbd7003832fe0642fcffaecb70ebb2eb27c
chinuteja <chinuteja2008@msitprogram.net>
2018-11-12 19:40:33 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/M7/1. MST/KruskalMST.java
+++ practice/M7/1. MST/KruskalMST.java
@@ -11,12 +11,11 @@ public class KruskalMST {
      * { // edges in MST}
      */
     private Queue<Edge> mst = new Queue<Edge>();  
-
     /**.
      * Compute a minimum spanning tree (or forest) of an edge-weighted graph.
      * @param G the edge-weighted graph
      */
-    public KruskalMST(EdgeWeightedGraph G) {
+    public KruskalMST(final EdgeWeightedGraph G) {
         // more efficient to build heap by passing array of edges
         MinPQ<Edge> pq = new MinPQ<Edge>();
         for (Edge e : G.edges()) {
@@ -35,12 +34,8 @@ public class KruskalMST {
                 weight += e.weight();
             }
         }
-
-        // check optimality conditions
-        //assert check(G);
     }
-
-    /**
+    /**.
      * Returns the edges in a minimum spanning tree (or forest).
      * TIme complexity is E as it iterates through all the edges
      * @return the edges in a minimum spanning tree (or forest) as
@@ -49,8 +44,7 @@ public class KruskalMST {
     public Iterable<Edge> edges() {
         return mst;
     }
-
-    /**
+    /**.
      * Returns the sum of the edge weights in a minimum 
      * spanning tree (or forest).
      * Time complexity is constant as each statement is executed only once
@@ -60,70 +54,5 @@ public class KruskalMST {
     public double weight() {
         return weight;
     }
-    /**.
-     * // check optimality conditions
-     *takes time proportional to E V lg* V
-     * @param      G     { edge weighted graph }
-     *
-     * @return     { returns true if  graph is edge weighted
-     * else returns flalse }
-     */
-    // private boolean check(EdgeWeightedGraph G) {
-
-    //     // check total weight
-    //     double total = 0.0;
-    //     for (Edge e : edges()) {
-    //         total += e.weight();
-    //     }
-    //     if (Math.abs(total - weight()) > FLOATING_POINT_EPSILON) {
-    //         return false;
-    //     }
-
-    //     // check that it is acyclic
-    //     UF uf = new UF(G.V());
-    //     for (Edge e : edges()) {
-    //         int v = e.either(), w = e.other(v);
-    //         if (uf.connected(v, w)) {
-    //             System.err.println("Not a forest");
-    //             return false;
-    //         }
-    //         uf.union(v, w);
-    //     }
-
-    //     // check that it is a spanning forest
-    //     for (Edge e : G.edges()) {
-    //         int v = e.either(), w = e.other(v);
-    //         if (!uf.connected(v, w)) {
-    //             System.err.println("Not a spanning forest");
-    //             return false;
-    //         }
-    //     }
-
-    //     // check that it is a minimal spanning forest
-    //     // (cut optimality conditions)
-    //     for (Edge e : edges()) {
-
-    //         // all edges in MST except e
-    //         uf = new UF(G.V());
-    //         for (Edge f : mst) {
-    //             int x = f.either(), y = f.other(x);
-    //             if (f != e) uf.union(x, y);
-    //         }
-            
-    //         // check that e is min weight edge in crossing cut
-    //         for (Edge f : G.edges()) {
-    //             int x = f.either(), y = f.other(x);
-    //             if (!uf.connected(x, y)) {
-    //                 if (f.weight() < e.weight()) {
-    //                     return false;
-    //                 }
-    //             }
-    //         }
-
-    //     }
-
-    //     return true;
-    // }
-
 }
 

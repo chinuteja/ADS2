dc94aae1136bf609d5128f29ade72e99b9180ba6
chinuteja <chinuteja2008@msitprogram.net>
2018-11-13 18:03:43 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/M14/Assignment-1/Solution.java
+++ practice/M14/Assignment-1/Solution.java
@@ -1,33 +1,45 @@
+/**.
+ * imports Scanner package
+ */
 import java.util.Scanner;
+/**.
+ * Class for solution.
+ * @author Teja
+ */
+public final class Solution {
+    /**.
+     * Constructs the object for SOlution
+     */
+    private Solution() {
 
-public class Solution {
-	public static void main(String[] args) {
-		String[] words = loadWords();
-		//Your code goes here..
-		// for (int i = 0;i < words.length;i++ ) {
-		// 	System.out.println(words[i]);
-			
-		// }
-		Scanner scan = new Scanner(System.in);
-		String prefix = scan.nextLine();
-		int count = 0;
-		TST<Integer> tst = new TST<>();
-		for (int i = 0; i < words.length; i++) {
-			for (int j = 0; j < words[i].length(); j++ ) {
-				tst.put(words[i].substring(j), j);
-			}
-
-		}
-		for (String str : tst.keysWithPrefix(prefix) ) {
-			System.out.println(str);
-		}
-
-
-	}
-
-	public static String[] loadWords() {
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-	}
+    }
+    /**.
+     * main function which performs the operations
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        String[] words = loadWords();
+        //Your code goes here..
+        // for (int i = 0;i < words.length;i++ ) {
+        //  System.out.println(words[i]);
+            
+        // }
+        Scanner scan = new Scanner(System.in);
+        String prefix = scan.nextLine();
+        TST<Integer> tst = new TST<>();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++ ) {
+                tst.put(words[i].substring(j), j);
+            }
+        }
+        for (String str : tst.keysWithPrefix(prefix) ) {
+            System.out.println(str);
+        }
+    }
+    public static final String[] loadWords() {
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+    }
 }

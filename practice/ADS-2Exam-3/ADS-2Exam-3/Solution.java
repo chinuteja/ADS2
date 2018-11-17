511830110d358a4b6e7b82d8217e8210472f81a4
chinuteja <chinuteja2008@msitprogram.net>
2018-11-17 11:27:03 +0530

    "ads2_m18_a1_1: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/ADS-2Exam-3/ADS-2Exam-3/Solution.java
+++ practice/ADS-2Exam-3/ADS-2Exam-3/Solution.java
@@ -84,7 +84,6 @@ public class Solution {
 		In in = new In(file);
 		return in.readAllStrings();
 	}
-
 	public static BinarySearchST<String, Integer> loadDictionary(String file) {
 		BinarySearchST<String, Integer>  st = new BinarySearchST<String, Integer>();
 		// your code goes here
@@ -103,19 +102,27 @@ public class Solution {
 }
 
 class T9 {
-
+	TST<Integer> tst;
 	public T9(BinarySearchST<String, Integer> st) {
 		// your code goes here
+		tst = new TST<Integer>();
+		for (int i = 0; i < st.size(); i++) {
+			String temp = st.select(i);
+			tst.put(temp, i);
+		}
+
 	}
 
 	// get all the prefixes that match with given prefix.
 	public Iterable<String> getAllWords(String prefix) {
 		// your code goes here
-		return null;
+		return tst.keysWithPrefix(prefix);
+		//return null;
 	}
 
 	public Iterable<String> potentialWords(String t9Signature) {
 		// your code goes here
+
 		return null;
 	}
 
